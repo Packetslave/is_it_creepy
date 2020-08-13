2642e78d8d955c9a3f86cc29dc22800225df2b81
blanders <brian@packetslave.com>
2020-08-12 19:45:11 -0700

    cleanup and functional

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ u("form").handle("submit", function (e) {
   var min_age = older / 2 + 7;
   var answer = u("#answer");
 
-  answer.removeClass("wtf");
-  answer.removeClass("yes");
-  answer.removeClass("borderline");
-  answer.removeClass("nope");
+  answer.removeClass("alert-danger");
+  answer.removeClass("alert-warning");
+  answer.removeClass("alert-success");
 
   if (younger < 13 || older < 16) {
     answer.text("seriously?! yes!");
-    answer.addClass("wtf");
+    answer.addClass("alert-danger");
   } else if (Math.abs(min_age - younger) <= 1) {
     answer.text("borderline");
-    answer.addClass("borderline");
+    answer.addClass("alert-warning");
   } else if (younger > min_age) {
+    answer.addClass("alert-success");
     answer.text("nope");
   } else {
     answer.text("yes");
